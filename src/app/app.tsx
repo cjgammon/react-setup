7fdb01a30fec1a3816eecd98a9322bbb6600894d
cjgammon <gammon@adobe.com>
2020-04-15 11:22:21 -0500

    initial add

---
+++ src/app/app.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default class App extends React.Component<{}> {
+
+  render() {
+    return (
+      <h1>
+          this is my app
+      </h1>
+    );
+  }
+  
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import './index.less';
+
+import App from "./app/app";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
